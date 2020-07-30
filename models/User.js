1e19437945b10b16ac89a78623e107a5cdf6265d
kleyton-ohare <kleyton.ohare@gmail.com>
2020-07-30 20:49:57 +0100

    defined a collection name

--- models/User.js
+++ models/User.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: String
 });
+const collectionName = 'names';
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Users', userSchema, collectionName);
